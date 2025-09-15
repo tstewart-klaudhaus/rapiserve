004991028d76936962ee23a51a6d833dcdc9d653
tstewart <tstewart@klaudhaus.com>
2025-09-15 10:50:25 +1000

    Display sample site with RPS launcher.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<iframe src="https://bunnings.com.au"></iframe>
+
+<div class="rps-launcher">
+	<i class="bi bi-chat-text"></i>
+</div>
+
+<style>
+	iframe {
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import devtoolsJson from 'vite-plugin-devtools-json';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [sveltekit(), devtoolsJson()],
+	test: {
+		expect: { requireAssertions: true },
+		projects: [
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
+		]
+	}
+});
